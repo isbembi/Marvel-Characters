a3b5d96748dd7c6c50c84ac00cce1f10f5b12f40
Bermet <iskenderova.bermet19@gmail.com>
2024-12-10 18:17:06 +0600

    Searching bar created

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <input
+      type="text"
+      placeholder="Search characters..."
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+    />
+  );
+};
+
+export default SearchBar;
